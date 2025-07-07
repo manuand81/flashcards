1b54e8ef71ff7b437aa9b03fd6a521b202e69bec
manuand81 <emanuelle.quintana@escola.pr.gov.br>
2025-07-07 12:14:03 -0300

    anjn

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// Função para alternar entre a pergunta e a resposta ao clicar no cartão
-function toggleResposta(cartao) {
-    const resposta = cartao.querySelector('.cartao__conteudo__resposta');
-    
-    // Verifica se a resposta está visível
-    if (resposta.style.display === "block" || cartao.classList.contains("show")) {
-        // Se estiver visível, esconda a resposta e remova a classe 'show'
-        cartao.classList.remove("show");
-    } else {
-        // Se não estiver visível, mostre a resposta e adicione a classe 'show'
-        cartao.classList.add("show");
-    }
+// Função para virar o cartão e mostrar a resposta
+function flipCard(cartao) {
+    cartao.classList.toggle("flipped"); // Adiciona ou remove a classe 'flipped' para virar o cartão
 }
